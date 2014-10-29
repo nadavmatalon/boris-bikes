7d5383c3d19db4e4e127476f644fa6f75e268496
Nadav <nadav.matalon@gmail.com>
2014-10-29 15:19:02 +0000

    code refactoring of Van tests

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,10 +2,14 @@ require "van"
 
 describe "Van" do 
 
-	let(:van) { Van.new(:capacity => 20) }
+	let(:van) { Van.new }
 
-	it "should allow setting default capacity on initialising" do    
-    expect(van.capacity).to eq (20)
+	it "should have default capacity of 10 bikes" do
+		expect(van.capacity).to eq 10
+	end
+
+	it "allows setting of it's capacity on initialization" do
+		expect(Van.new(capacity: 20).capacity).to eq 20
 	end
 
 end
