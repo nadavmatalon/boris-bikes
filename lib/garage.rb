9cc38b8a432a875189e0672db13c09c874d0994e
Nadav <nadav.matalon@gmail.com>
2014-10-29 13:09:06 +0000

    cleaned up Garage class code

--- lib/garage.rb
+++ lib/garage.rb
@@ -6,17 +6,14 @@ class Garage
 
 	def initialize(options = {})
 		self.capacity=(options.fetch(:capacity, self.capacity))
-		# self.bike_count = options.fetch(:bike_count, bike_count)
 		@docked_bikes ||= 0
 	end
 
 	def fix(bike)
-		if bike.broken?
-			bike.fix
-		end
+		bike.fix if bike.broken?
 	end
 
 	def fix_all_broken_bikes 
-		bikes.each {|bike| fix(bike) if (bike.broken?)}
+		bikes.each { |bike| fix(bike) if bike.broken? }
 	end
 end
